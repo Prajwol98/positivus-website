25001d659edf9816b0523580fe3638feb812b54d
Prajwol Mainali <prajwolmainali96@gmail.com>
2024-09-01 21:55:12 +0545

    naavbar complete

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,7 +1,62 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div className="text-2xl text-red-400">Navbar</div>;
+  const datas = [
+    {
+      name: "Home",
+      link: "/",
+    },
+    {
+      name: "Features",
+      link: "/features",
+    },
+    {
+      name: "Benefits",
+      link: "/Benefits",
+    },
+    {
+      name: "pricing",
+      link: "/pricing",
+    },
+    {
+      name: "contact",
+      link: "/contact",
+    },
+  ];
+  return (
+    <>
+      <div className="flex justify-between items-center bg-primary p-[20px] ">
+        <div>
+          <a href="#">
+            <img
+              src="../public/companylogo.png"
+              alt="img"
+              className="h-[50] w-[50]"
+            />
+          </a>
+        </div>
+        <div className="flex gap-[31px] text-white font-poppins">
+          {datas.map((data) => {
+            return (
+              <ul>
+                <li>
+                  <a href="">{data.name}</a>
+                </li>
+              </ul>
+            );
+          })}
+        </div>
+        <div>
+          <button className=" text-white font-bold mr-[25px] rounded-full font-poppins">
+            Login
+          </button>
+          <button className="bg-white font-poppins text-black font-bold py-[12px] px-[20px] rounded-full">
+            Get Started
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#022C22",
+      },
+    },
+    fontFamily: {
+      poppins: ["Poppins"],
+    },
   },
   plugins: [],
 };
